58bb988a6588fbe472d160f2ce0439e1621d4a85
xuke918 <xuke3070@gmail.com>
2016-03-18 18:38:43 -0400

    Added is clicked to main css and matched toggle.

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var $form = $('form');
+var $input = $('#list-input');
+var $list = $('.list');
+
+$('form').on('submit', function () {
+  var $li = $('</li>');
+
+  e.preventDefault();
+  $li.html($input.val());
+  $list.append($li);
+  $input.val('');
+
+});
+
+$('ul').on('click', 'li', function (e) {
+  $(this).toggleClass('is-clicked');
+});
